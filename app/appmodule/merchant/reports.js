c147dc9a46692699fbe759e5d1890585e2b0ddbd
pratikn <pratikway.90@gmail.com>
2017-06-24 20:16:52 +0530

    Bill Reports Committed

--- app/appmodule/merchant/reports.js
+++ app/appmodule/merchant/reports.js
@@ -3,10 +3,18 @@ var rs = require("gen").res;
 var globals = require("gen").globals;
 var socket = require("socket");
 
-var dayend = module.exports = {};
+var rpt = module.exports = {};
 
-dayend.getDayEndReports = function getDayEndReports(req, res, done) {
+rpt.getDayEndReports = function getDayEndReports(req, res, done) {
     db.callProcedure("select " + globals.merchant("funget_rpt_dayend") + "($1,$2::json);", ['de', req.body], function(data) {
+        rs.resp(res, 200, data.rows);
+    }, function(err) {
+        rs.resp(res, 401, "error : " + err);
+    }, 1)
+}
+
+rpt.getInvoiceDetails = function getInvoiceDetails(req, res, done) {
+    db.callProcedure("select " + globals.merchant("funget_rpt_invoice") + "($1,$2::json);", ['inv', req.body], function(data) {
         rs.resp(res, 200, data.rows);
     }, function(err) {
         rs.resp(res, 401, "error : " + err);
