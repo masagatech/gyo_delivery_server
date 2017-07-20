9cbe71acb7874c0484881347ff2c738df0c8408d
llc <pratikway.90@gmail.com>
2017-07-20 21:54:49 +0530

    remove mongodb changes

--- app/appmodule/merchant/mobile.js
+++ app/appmodule/merchant/mobile.js
@@ -8,18 +8,18 @@ var livebeat = module.exports = {};
 
 livebeat.savelivebeat = function savelivebeat(req, res, done) {
 
-    console.log(req.query);
-    if(req.query.tripid!='0'){
-        MongoClient.connect(url, function(err, db) {
-        if (err) throw err;
-        var trip_details = req.query;
-            db.collection("tripdetails").insertOne( trip_details, function(err, res) {
-                if (err) throw err;
-                console.log("1 record inserted");
-                db.close();
-            });
-        });
-    }
+   
+    // if(req.query.tripid!='0'){
+    //     MongoClient.connect(url, function(err, db) {
+    //     if (err) throw err;
+    //     var trip_details = req.query;
+    //         db.collection("tripdetails").insertOne( trip_details, function(err, res) {
+    //             if (err) throw err;
+    //             console.log("1 record inserted");
+    //             db.close();
+    //         });
+    //     });
+    // }
     
     db.callFunction("select " + globals.merchant("api_funsave_riderlive") + "($1::json);", [req.query], function(data) {
         rs.resp(res, 200, data.rows[0].api_funsave_riderlive);
