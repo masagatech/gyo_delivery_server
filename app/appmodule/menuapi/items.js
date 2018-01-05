10423aa685bea3afc5568af6f842acaf5c84236f
llc <pratikway.90@gmail.com>
2018-01-05 12:21:37 +0530

    commited

--- app/appmodule/menuapi/items.js
+++ app/appmodule/menuapi/items.js
@@ -5,25 +5,25 @@ var globals = require("gen").globals;
 var item = module.exports = {};
 
 item.getItemDetails = function getItemDetails(req, res, done) {
-    db.callProcedure("select " + globals.menuschema("funget_itemdetails") + "($1,$2::json);", ['item', req.body], function(data) {
+    db.callProcedure("select " + globals.menuschema("funget_itemdetails") + "($1,$2::json);", ['item', req.body], function (data) {
         rs.resp(res, 200, data.rows);
-    }, function(err) {
+    }, function (err) {
         rs.resp(res, 401, "error : " + err);
     }, 1)
 }
 
 item.saveItemInfo = function saveItemInfo(req, res, done) {
-    db.callFunction("select " + globals.menuschema("funsave_iteminfo") + "($1::json);", [req.body], function(data) {
+    db.callFunction("select " + globals.menuschema("funsave_iteminfo") + "($1::json);", [req.body], function (data) {
         rs.resp(res, 200, data.rows);
-    }, function(err) {
+    }, function (err) {
         rs.resp(res, 401, "error : " + err);
     })
 }
 
-item.saveMultiItemInfo = function saveMultiItemInfo(req, res, done) {
-    db.callFunction("select " + globals.menuschema("funsave_multiitems") + "($1::json);", [req.body], function(data) {
-        rs.resp(res, 200, data.rows);
-    }, function(err) {
-        rs.resp(res, 401, "error : " + err);
+item.saveMultiItemInfo = function saveMultiItemInfo(req, callback) {
+    db.callFunction("select " + globals.menuschema("funsave_multiitems") + "($1::json);", [req], function (data) {
+        callback(data.rows);
+    }, function (err) {
+        callback("error : " + err);
     })
 }
