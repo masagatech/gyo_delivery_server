213b5416db44240ca7cfa14ecb20a77c7245c08d
vivekpandey12 <vivek.pandey5188@gmail.com>
2018-01-24 10:00:30 +0530

    committed

--- app/appmodule/merchant/orderdashboard.js
+++ app/appmodule/merchant/orderdashboard.js
@@ -5,7 +5,7 @@ var globals = require("gen").globals;
 var order = module.exports = {};
 
 order.postOrderDash = function getOrderDash(req, res, done) {
-        db.callProcedure("select " + globals.merchant("funget_orderdashboard") + "($1,$2,$3::json);", ['dash1', 'dash2',req.body ], function(data) {
+    db.callProcedure("select " + globals.merchant("funget_orderdashboard") + "($1,$2,$3::json);", ['dash1', 'dash2', req.body], function(data) {
         rs.resp(res, 200, data.rows);
     }, function(err) {
         rs.resp(res, 401, "error : " + err);
@@ -13,22 +13,20 @@ order.postOrderDash = function getOrderDash(req, res, done) {
 }
 
 order.getOrderDash = function getOrderDash(req, res, done) {
-      req.body = req.query;
-      order.postOrderDash (req, res, done);
+    req.body = req.query;
+    order.postOrderDash(req, res, done);
 }
 
 order.getOrderDetails = function getOrderDetails(req, res, done) {
-    if(req.query.flag == undefined){
-        
+    if (req.query.flag == undefined) {
+
         rs.resp(res, 401, "invalid input parameters flag");
         return;
     }
 
-    db.callProcedure("select " + globals.merchant("funget_orderdashdetails") + "($1,$2::json);", ['dashdetail', req.query ], function(data) {
+    db.callProcedure("select " + globals.merchant("funget_orderdashdetails") + "($1,$2::json);", ['dashdetail', req.query], function(data) {
         rs.resp(res, 200, data.rows);
     }, function(err) {
         rs.resp(res, 401, "error : " + err);
     }, 1)
 }
-
-
