c2dc516040c0ea7a951af134d109e3884fb575b9
vivekpandey12 <vivek.pandey5188@gmail.com>
2018-05-14 13:32:06 +0530

    committed

--- app/appmodule/reportsapi/menuorder.js
+++ app/appmodule/reportsapi/menuorder.js
@@ -13,13 +13,18 @@ var reportsapi = require("../../reports/apis/reports.js");
 order.getMenuOrderReports = function getMenuOrderReports(req, res, done) {
     db.callProcedure("select " + globals.menuschema("funget_rpt_orderdetails") + "($1,$2,$3::json);", ['mord1', 'mord2', req.query], function(data) {
         var url = "";
-        url = "order/kotsleep.html";
+
+        if (req.query.flag == "kot_sleep") {
+            url = "order/kotsleep.html";
+        } else {
+            url = "invoice/menuinvoicerpt.html";
+        }
 
         download(req, res, {
             data: data.rows[0],
             data1: data.rows[1],
             params: req.query
-        }, { 'all': url }, reportsapi.getReports);
+        }, { 'all': url, 'headerfooter': 'no' }, reportsapi.getReports);
     }, function(err) {
         rs.resp(res, 401, "error : " + err);
     }, 2)
